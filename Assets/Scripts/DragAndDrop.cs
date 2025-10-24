8126a56e0be8042f2bb78cb145cc0b4870e79588
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-10-24 16:50:19 -0700

    draggable circle (physics still wonky)

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DragAndDrop : MonoBehaviour
+{
+    [SerializeField] private InputAction mouseClick; // i didn't even know you could do this
+    public float mouseDragSpeed = 10f;
+
+    private Camera _mainCam;
+    private WaitForFixedUpdate waitForFixedUpdate = new WaitForFixedUpdate();
+
+    private void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        mouseClick.Enable();
+        mouseClick.performed += MousePressed;
+    }
+
+    private void OnDisable()
+    {
+        mouseClick.performed -= MousePressed;
+        mouseClick.Disable();
+    }
+
+    private void MousePressed(InputAction.CallbackContext context)
+    {
+        RaycastHit2D hit = Physics2D.GetRayIntersection(_mainCam.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        if (hit.collider != null && (hit.collider.gameObject.CompareTag("Draggable")))
+        {
+            StartCoroutine(DragUpdate(hit.collider.gameObject));
+        }
+    }
+
+    private IEnumerator DragUpdate(GameObject selectedObject)
+    {
+        float initialDistance = Vector3.Distance(selectedObject.transform.position, _mainCam.transform.position);
+        Rigidbody2D rb = selectedObject.GetComponent<Rigidbody2D>();
+        while (mouseClick.ReadValue<float>() != 0f)
+        {
+            Ray ray = _mainCam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            Vector3 direction = ray.GetPoint(initialDistance) - selectedObject.transform.position;
+            rb.linearVelocity = direction * mouseDragSpeed;
+            yield return waitForFixedUpdate;
+        }
+    }
+}
