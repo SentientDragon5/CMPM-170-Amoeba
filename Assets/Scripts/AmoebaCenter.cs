72e2064a7afddcfc9052c501a064dd5e21f75eee
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-10-25 13:23:51 -0700

    bezier curves

--- Assets/Scripts/AmoebaCenter.cs
+++ Assets/Scripts/AmoebaCenter.cs
@@ -1,48 +1,155 @@
 using System.Collections.Generic;
-using System.Data.Common;
+using System.Threading;
 using UnityEngine;
 
 public class AmoebaCenter : MonoBehaviour
 {
-    public int resolution = 5;
-    
-    public GameObject[] amoebePoints;
-    private List<Transform> points = new List<Transform>();
+    public int lineResolution = 20;
+    public int raycastResolution = 80;
+    public bool drawLines = false;
+    public LayerMask amoebaPointLayer;
+    public EdgeCollider2D edgeCollider;
 
-    private void OnValidate()
-    {
-        amoebePoints = GameObject.FindGameObjectsWithTag("Draggable");
-    }
+    public List<Transform> points = new List<Transform>();
+    private LineRenderer lr;
+
+    private List<Vector2> colliderPoints = new List<Vector2>();
 
     private void Awake()
     {
+        lr = GetComponent<LineRenderer>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
     }
 
     private void Start()
     {
-        foreach (GameObject obj in amoebePoints)
-        {
-            points.Add(obj.transform);
-        }
+        ScanPoints();
+        lr.positionCount = lineResolution * points.Count;
+        CreateLine();
     }
 
     private void Update()
     {
-        Vector2 centroid = CalculateCentroid();
-        transform.position = centroid;
+        transform.position = CalculateCentroid();
+        CreateLine();
+
     }
 
     private Vector2 CalculateCentroid()
     {
         float xSum = 0f;
         float ySum = 0f;
-        for (int i = 0; i < amoebePoints.Length; i++)
+        for (int i = 0; i < points.Count; i++)
         {
             xSum += points[i].transform.position.x;
             ySum += points[i].transform.position.y;
         }
 
-        return new Vector2(xSum/ amoebePoints.Length, ySum/ amoebePoints.Length);
+        return new Vector2(xSum/ points.Count, ySum/ points.Count);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if (drawLines)
+        {
+            for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+            {
+                Gizmos.DrawRay(transform.position, Vector3.Lerp(new Vector3(1, 0, 0), new Vector3(0, 1, 0), t) * 100f);
+            }
+            for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+            {
+                Gizmos.DrawRay(transform.position, Vector3.Lerp(new Vector3(0, 1, 0), new Vector3(-1, 0, 0), t) * 100f);
+            }
+            for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+            {
+                Gizmos.DrawRay(transform.position, Vector3.Lerp(new Vector3(-1, 0, 0), new Vector3(0, -1, 0), t) * 100f);
+            }
+            for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+            {
+                Gizmos.DrawRay(transform.position, Vector3.Lerp(new Vector3(0, -1, 0), new Vector3(1, 0, 0), t) * 100f);
+            }
+        }
+    }
+
+    // yes i know this is bad
+    // no i will not try to optimize it
+    private void ScanPoints()
+    {
+        for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.Lerp(new Vector3(1, 0, 0), new Vector3(0, 1, 0), t), 100f, amoebaPointLayer);
+           
+            if (hit && hit.collider.CompareTag("Draggable") && !points.Contains(hit.transform))
+            {
+                points.Add(hit.transform);
+            }
+        }
+        for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.Lerp(new Vector3(0, 1, 0), new Vector3(-1, 0, 0), t), 100f, amoebaPointLayer);
+            if (hit && hit.collider.CompareTag("Draggable") && !points.Contains(hit.transform))
+            {
+                points.Add(hit.transform);
+            }
+        }
+        for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.Lerp(new Vector3(-1, 0, 0), new Vector3(0, -1, 0), t), 100f, amoebaPointLayer);
+            if (hit && hit.collider.CompareTag("Draggable") && !points.Contains(hit.transform))
+            {
+                points.Add(hit.transform);
+            }
+        }
+        for (float t = 0; t <= 1; t += 1f / (raycastResolution / 4))
+        {
+           RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.Lerp(new Vector3(0, -1, 0), new Vector3(1, 0, 0), t), 100f, amoebaPointLayer);
+            if (hit && hit.collider.CompareTag("Draggable") && !points.Contains(hit.transform))
+            {
+                points.Add(hit.transform);
+            }
+        }
+    }
+
+    private void CreateLine()
+    {
+        int count = 0;
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (i == points.Count - 1)
+            {
+                for (float t = 0; t < 1; t += 1f / lineResolution)
+                {
+                    Vector2 point = QuadraticBezierCurve(points[i].transform, transform, points[0], t);
+                    lr.SetPosition(count, point);
+                    colliderPoints.Add(point);
+                    count++;
+                }
+            }
+            else
+            {
+                for (float t = 0; t < 1; t += 1f / lineResolution)
+                {
+                    Vector2 point = QuadraticBezierCurve(points[i].transform, transform, points[i + 1], t);
+                    lr.SetPosition(count, point);
+                    colliderPoints.Add(point);
+                    count++;
+                }
+            }
+        }
+    }
+
+    private void CreateCollider()
+    {
+        edgeCollider.points = colliderPoints.ToArray();
+    }
+
+    // quadratic bezier curve formula from wiki
+    // P1 + (1-t)^2 * (P0 - P1) + t^2(P2 - P1), 0 <= t <= 1
+    private Vector2 QuadraticBezierCurve(Transform p0, Transform p1, Transform p2, float t)
+    {
+        return p1.position + Mathf.Pow(1 - t, 2) * (p0.position - p1.position) + Mathf.Pow(t, 2) * (p2.position - p1.position);
     }
 
 }
--- Assets/Scripts/AmoebaPoint.cs
+++ Assets/Scripts/AmoebaPoint.cs
@@ -1,18 +1,21 @@
-using UnityEngine;
-using System.Linq;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using UnityEngine;
 
 public class AmoebaPoint : MonoBehaviour
 {
     public AmoebaCenter center;
     public List<GameObject> neighbors = new List<GameObject>();
 
-    public float distance = 4;
-    public float speed = 2f;
+    public float distanceCenter = 4;
+    public float distanceNeighbors = 4;
+    public float speedAwayFromCenter = 2f;
+    public float speedTowardsEachOther = 0.2f;
     public bool isBeingDragged = false;
 
     private Rigidbody2D rb;
-    private LineRenderer lr;
+
 
     private void OnValidate()
     {
@@ -26,45 +29,33 @@ public class AmoebaPoint : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        lr = GetComponent<LineRenderer>();
     }
 
     private void Start()
     {
-
-        lr.positionCount = 3;
         neighbors.Clear();
         GetClosestTwoPoints();
-
     }
 
-    private void Update()
-    {
-        lr.SetPosition(0, neighbors[0].transform.position);
-        lr.SetPosition(1, transform.position);
-        lr.SetPosition(2, neighbors[1].transform.position);
-        //for (int i = 0; i < lr.positionCount/2; i++)
-        //{
-        //    lr.SetPosition(i, Vector3.Lerp(neighbors[0].transform.position, transform.position, (1/(float)lr.positionCount)*i));
-        //}
-        //int count = 0;
-        //for (int i = lr.positionCount / 2; i < lr.positionCount; i++)
-        //{
-        //    lr.SetPosition(i, Vector3.Slerp(transform.position, neighbors[1].transform.position, (1 / (float)lr.positionCount) * count));
-        //    count++;
-        //}
-    }
 
     // movement stuff
     private void FixedUpdate()
     {
-        if (isBeingDragged) return;
+        // moves the point away from the center
+        if (Vector2.Distance(transform.position, center.transform.position) < distanceCenter)
+        {
+            Vector3 direction = transform.position - center.transform.position;
+            rb.AddForce(direction * speedAwayFromCenter);
+        }
+        //if (isBeingDragged) return;
+
+        // moves this point towards other points
         foreach (GameObject point in neighbors)
         {
-            if (Vector2.Distance(point.transform.position, transform.position) > distance)
+            if (Vector2.Distance(point.transform.position, transform.position) > distanceNeighbors)
             {
                 Vector3 direction = point.transform.position - transform.position;
-                rb.AddForce(direction * speed);
+                rb.AddForce(direction * speedTowardsEachOther);
             }
         }
     }
@@ -83,12 +74,4 @@ public class AmoebaPoint : MonoBehaviour
         neighbors.Add(list[1]);
     }
 
-    private void PrintArray(GameObject[] array)
-    {
-        for (int i = 0; i < array.Length; i++)
-        {
-            Debug.Log(array[i].name);
-        }
-        Debug.Log("----------------");
-    }
 }
