fbbd06c167b77b00f6db5abdebade2540ecaaf77
Logan <34078184+SentientDragon5@users.noreply.github.com>
2025-10-25 22:22:31 -0700

    working circles

---
+++ Assets/Shaders/SDF2.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class SDF2 : MonoBehaviour
+{
+    // x, y = position (0-1 UV space)
+    // z = unused
+    // w = radius
+    public Vector4[] points = new Vector4[1];
+
+    private Material mat;
+    private ComputeBuffer pointsBuffer;
+
+    void OnEnable()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        UpdateBuffer();
+    }
+
+    void UpdateBuffer()
+    {
+        if (points == null) return;
+        int count = points.Length;
+        
+        if (pointsBuffer == null || pointsBuffer.count != count)
+        {
+            if (pointsBuffer != null)
+            {
+                pointsBuffer.Release();
+            }
+            pointsBuffer = new ComputeBuffer(Mathf.Max(1, count), sizeof(float) * 4);
+        }
+
+        if (count > 0)
+        {
+            pointsBuffer.SetData(points);
+        }
+
+        mat.SetBuffer("_Points", pointsBuffer);
+        mat.SetInt("_PointCount", count);
+    }
+
+    void OnDisable()
+    {
+        if (pointsBuffer != null)
+        {
+            pointsBuffer.Release();
+            pointsBuffer = null;
+        }
+    }
+}
